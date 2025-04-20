194e8be2823bd1451213bd33067686f6205d91ea
aarib <aarib9892@gmail.com>
2025-04-20 21:23:13 +0530

    webhhook testing debug...

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -37,6 +37,7 @@ export async function POST(req: Request) {
       };
 
       const newUser = await createUser(user);
+      console.log(newUser,"newUser")
 
       // Set public metadata
       if (newUser) {
